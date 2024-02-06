04a5038f128b06e64ef2dee74cbb6807af8c43b5
SylarZzz <sylarzhang926@hotmail.com>
2024-02-06 13:01:26 -0800

    added front-end code

---
+++ wpif/src/components/City.tsx
@@ -0,0 +1,33 @@
+import axios from "axios";
+import { useEffect } from "react";
+import { useState } from "react";
+
+function City() {
+
+    const [response, setresponse] = useState([]);
+
+    // responsible for calling the api
+    const getdata = async () => {
+        try {
+            const res = await axios.get('https://localhost:5092/api/v1/city/1'); // using axios to do GET request
+            setresponse(res.data);
+        } catch (err) {
+            console.log("error");
+        }
+    };
+
+    useEffect( () => {
+        getdata();
+    } ,[]);
+
+    if (response) {
+        return (<h1> My city {JSON.stringify(response)} </h1>);
+    }
+    else {
+        return (<h1> My city </h1>);
+    }
+
+  }
+  
+  export default City
+  
